80a1aebe156447a35eefea3201a8d6a7b23f1acf
kxz <63444697+kxzazz@users.noreply.github.com>
2024-03-24 02:55:18 +0530

    Time: 29 ms (50.41%) | Memory: 24.7 MB (36.81%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> hashRecord;
-        for(string str: strs){
-            string hash = str;
-            sort(hash.begin(), hash.end());
-            hashRecord[hash].push_back(str);
+        unordered_map<string, vector<string>> mp;
+        for (string s : strs) {
+            string t = s;
+            sort(t.begin(), t.end());
+            mp[t].push_back(s);
         }
-
-        vector<vector<string>> ans;
-        for(auto record: hashRecord){
-            ans.push_back(record.second);
+        vector<vector<string>> anagrams;
+        for (auto p : mp) {
+            anagrams.push_back(p.second);
         }
-        return ans;
-        
+        return anagrams;
     }
 };
